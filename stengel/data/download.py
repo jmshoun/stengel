e00bf5292141631ce942e19a0bb88555ab5eabf7
Mark Shoun <mark.shoun@gmail.com>
2017-04-02 15:50:38 -0400

    Add functions for downloading data
    Stengel is heavily reliant on external data sources for model training.
    The functions added here automate the process of downloading the data
    from third-party websites.
    
    This is also the first non-trivial commit, so it sets up the project
    structure. stengel/ is a package defining functions and classes used by
    the rest of the project; test/ contains automated unit tests for
    the stengel package.

---
+++ stengel/data/download.py
@@ -0,0 +1,132 @@
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+
+import os
+import zipfile
+import time
+import math
+import random
+import socket
+
+from six.moves import urllib
+import bs4
+
+
+def create_directory_if_needed(directory):
+    """Creates the specified directory if it doesn't already exist."""
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+def is_connected(test_connection=("www.google.com", 80)):
+    """Test if an internet connection is active and available.
+
+    Args:
+        test_connection: URL and port of the site to try to reach. Default is Google.
+    """
+    try:
+        socket.create_connection(test_connection)
+        return True
+    except OSError:
+        pass
+    return False
+
+
+def metered_delay(mean, sd):
+    """Decorator to add a stochastic delay to a function call.
+
+    Args:
+        mean: Mean delay, in log(seconds).
+        sd: Standard deviation of delay, in log(seconds).
+    Returns:
+        Evaluates the decorated function after a random delay chosen from a lognormal
+        distribution with the given parameters.
+    """
+    if sd <= 0:
+        raise ValueError("sd must be strictly positive.")
+
+    def wrap(func):
+        def wrapped_func(*args, **kwargs):
+            delay_time = math.exp(random.gauss(mean, sd))
+            time.sleep(delay_time)
+            return func(*args, **kwargs)
+        return wrapped_func
+    return wrap
+
+
+@metered_delay(-0.2, 0.2)
+def gameday_pitches(game_, destination_root="data/gameday"):
+    """Download the MLB GameDay PitchFx data associated with a given game.
+
+    Args:
+        game_: A Game object.
+        destination_root: Root of the path to save the GameDay file in.
+    Returns:
+        Saves the PitchFx data file inside
+        [destination_root]/[year_of_game]/[retrosheet_id].xml.
+    """
+
+    year, month, day = game_.metadata.year_month_day()
+    destination_path = os.path.join(destination_root, year)
+    create_directory_if_needed(destination_path)
+
+    source_url = game_.metadata.gameday_url() + "/inning/inning_all.xml"
+    destination_file = os.path.join(destination_path, game_.metadata.id_ + ".xml")
+    urllib.request.urlretrieve(source_url, destination_file)
+
+
+def retrosheet_players(destination_root="data/retrosheet"):
+    """Download basic information on every player in the Retrosheet database.
+
+    Retrosheet.org has an html page that contains a CSV with the first name, last name,
+    Retrosheet identifier, and MLB debut of every player in the database.
+
+    Args:
+        destination_root: Root of the path to save the file in.
+    Returns:
+        The name of the file that was created. Saves the Retrosheet file in 
+        [destination_root]/player_ids.csv.
+    """
+    source_site = "http://www.retrosheet.org/retroID.htm"
+    # There are a couple of blank lines at the top of the file that we want to ignore.
+    num_junk_rows = 2
+
+    create_directory_if_needed(destination_root)
+    source_page = urllib.request.urlopen(source_site).read()
+    source_soup = bs4.BeautifulSoup(source_page, "lxml")
+    # The data is on an html page with a bunch of other cruft; the actual data is inside the
+    # only <pre> tag in the page.
+    source_data = source_soup.pre.string
+    source_lines = source_data.split("\n")[num_junk_rows:]
+
+    destination_path = os.path.join(destination_root, "player_ids.csv")
+    with open(destination_path, "w") as outfile:
+        for line in source_lines:
+            outfile.write(line + "\n")
+    return destination_path
+
+
+def retrosheet_season(year, destination_root="data/retrosheet"):
+    """Download a full season of Retrosheet game files.
+
+    The season file is available as a zip archive; the archive is automatically unzipped
+    and the uncompressed files are saved in the specified folder.
+
+    Args:
+        year: Year of the season to download.
+        destination_root: Root of the path to save the files in.
+    Side Effects:
+        Saves all of the files in [destination_root]/[year].
+    """
+    source_site = "http://www.retrosheet.org/events/"
+    destination_path = os.path.join(destination_root, str(year))
+    create_directory_if_needed(destination_path)
+    
+    season_file = "{:1}eve.zip".format(year)
+    season_url = source_site + season_file
+    destination_file = os.path.join(destination_path, season_file)
+    urllib.request.urlretrieve(season_url, destination_file)
+    
+    with zipfile.ZipFile(destination_file) as season_zip:
+        season_zip.extractall(destination_path)
+    os.remove(destination_file)
---
+++ test/data/test_download.py
@@ -0,0 +1,78 @@
+import sys
+import os
+import time
+import math
+import shutil
+import unittest
+
+foo = os.path.abspath(os.path.join(sys.path[0], os.pardir))
+sys.path.insert(0, foo)
+
+import stengel.data.download as download
+
+TEST_DATA_PATH = "_data_download/"
+
+@download.metered_delay(-0.5, 0.001)
+def stub_function():
+    # A stub function used to test the functionality of metered_delay
+    return 42
+
+
+class TestMeteredDelay(unittest.TestCase):
+    def test_return_value(self):
+        self.assertEqual(stub_function(), 42, "Return value is incorrect.")
+
+    def test_delay_length(self):
+        start_time = time.time()
+        stub_function()
+        end_time = time.time()
+        time_delta = end_time - start_time
+        # Allowing ten sigma on either side of the expected delay time. This should
+        # essentially never fail due to random variance...
+        self.assertGreaterEqual(time_delta, math.exp(-0.51))
+        self.assertLessEqual(time_delta, math.exp(-0.49) + 0.001)
+
+
+class TestRetrosheetPlayers(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(TestRetrosheetPlayers, cls).setUpClass()
+        download.create_directory_if_needed(TEST_DATA_PATH)
+        cls.filename = download.retrosheet_players(TEST_DATA_PATH)
+
+    @classmethod
+    def tearDownClass(cls):
+        super(TestRetrosheetPlayers, cls).tearDownClass()
+        shutil.rmtree(TEST_DATA_PATH)
+
+    def setUp(self):
+        if not os.path.exists(self.filename):
+            self.fail("File did not download.")
+
+    def test_file_header(self):
+        with open(self.filename) as f:
+            content = f.readlines()
+            content = [line.strip() for line in content]
+            self.assertEqual(content[0], "LAST,FIRST,ID,DEBUT", "Header is missing.")
+
+    def test_file_length(self):
+        with open(self.filename) as f:
+            content = f.readlines()
+            # The file had >20K lines as of April 1, 2017; it will get bigger over time.
+            self.assertGreaterEqual(len(content), 20000, "File is too short.")
+
+    def test_file_format(self):
+        with open(self.filename) as f:
+            content = f.readlines()
+            commas_per_line = [self._count_commas_on_line(line) for line in content]
+            commas_per_line = [commas for commas in commas_per_line if commas > 0]
+            self.assertEqual(max(commas_per_line), 3, "Too many commas on at least one line.")
+            self.assertEqual(min(commas_per_line), 3, "Not enough commas on at least one line.")
+            self.assertGreaterEqual(len(commas_per_line), 20000, "Not enough formatted lines.")
+
+    @staticmethod
+    def _count_commas_on_line(line):
+        length = len(line.split(",")) - 1
+        if length == 1:
+            print(line)
+        return length
